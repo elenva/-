0c88bc753aa569ed7520194fec8f2363179f2eb6
yyh <yonghua.yuan@maxivetech.com>
2019-09-28 09:46:27 +0800

    添加搜索页面，调整路径

--- components/searchBar/bar/searchBar.js
+++ components/searchBar/bar/searchBar.js
@@ -7,7 +7,7 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    isInput:Boolean
   },
 
   /**
@@ -16,11 +16,22 @@ Component({
   data: {
 
   },
+  ready(){
+
+  },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    detail(){
+      wx.navigateTo({
+        url: '/components/searchBar/detail/detail',
+      })
+    },
+    confirm(e){
+      const {value} = e.detail;
+      this.triggerEvent('confirm',value)
+    }
   }
 })
---
+++ components/searchBar/detail/detail.js
@@ -0,0 +1,88 @@
+// components/searchBar/detail/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    history:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //获取storage
+    const _history = wx.getStorageSync('history');
+    if(_history) this.setData({history:_history})
+  },
+  confirm(e){
+    const value = e.detail
+    this.search(value)
+  },
+  search(v){
+    const {history} = this.data
+    const idx = history.findIndex(item => item === v);
+    if (idx !== -1) { 
+      history.splice(idx, 1);
+    }
+    history.unshift(v);
+    wx.setStorageSync('history', history)
+    this.setData({ history})
+  },
+  close(e){
+    const {index} = e.currentTarget.dataset;
+    const {history} = this.data;
+    history.splice(index,1);
+    wx.setStorageSync('history', history)
+    this.setData({history})
+   },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
